2807fb1ca4ff093be83d06a4fd8cab82eda4125e
tsquix <tsquix@gmail.com>
2024-03-18 21:49:58 +0100

    Dodaj pliki projektów.

---
+++ Web/Pages/Quiz/Summary.cshtml.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace BackendLab01.Pages;
+
+public class Summary : PageModel
+{   
+    private readonly IQuizUserService _userService;
+    
+    public int CorrectAnswerCount { get; set; }
+    public Summary(IQuizUserService userService)
+    {
+        _userService = userService;
+    }
+
+    public void OnGet(int quizId, int userId)
+    {
+        CorrectAnswerCount = _userService.CountCorrectAnswersForQuizFilledByUser(quizId, userId);
+    }
+}
